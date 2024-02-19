c92fefe5ec29883d37008aecca144f58d1500ac8
Iftekhar Hossain <learnwithiftekhar@gmail.com>
2024-02-20 00:35:19 +0600

    initial commit

---
+++ src/main/java/com/learnwithiftekhar/springMicrosoftSso/SpringMicrosoftSsoApplication.java
@@ -0,0 +1,13 @@
+package com.learnwithiftekhar.springMicrosoftSso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicrosoftSsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicrosoftSsoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnwithiftekhar/springMicrosoftSso/SpringMicrosoftSsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnwithiftekhar.springMicrosoftSso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicrosoftSsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
